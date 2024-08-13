8236c7c1001360819c00e69da7dd506c41394ad9
Kaito Udagawa <umireon@gmail.com>
2024-08-14 04:17:36 +0900

    Update main.tf

--- terraform/modules/hopscotch_audio_support/main.tf
+++ terraform/modules/hopscotch_audio_support/main.tf
@@ -16,8 +16,3 @@ resource "google_firebase_project" "default" {
 
 resource "google_identity_platform_config" "default" {
 }
-
-resource "google_firebase_hosting_site" "default" {
-  provider = google-beta
-  site_id  = var.project_id
-}
