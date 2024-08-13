58dbd8b22e2d694537269d52cc75ccf8aa3535a7
Kaito Udagawa <umireon@gmail.com>
2024-08-14 03:38:39 +0900

    Enable Cloud Resource Manager

--- terraform/modules/hopscotch_audio_support/main.tf
+++ terraform/modules/hopscotch_audio_support/main.tf
@@ -2,7 +2,14 @@ resource "google_project_service" "firebase" {
   service = "firebase.googleapis.com"
 }
 
+resource "google_project_service" "cloudresourcemanager" {
+  service = "cloudresourcemanager.googleapis.com"
+}
+
 resource "google_firebase_project" "default" {
-  provider   = google-beta
-  depends_on = [google_project_service.firebase]
+  provider = google-beta
+  depends_on = [
+    google_project_service.firebase,
+    google_project_service.cloudresourcemanager,
+  ]
 }
