f3e69abe0292b6eebb10b3ff577c2c341a2f47ed
Kaito Udagawa <umireon@gmail.com>
2024-08-15 00:36:58 +0900

    Update main.tf

--- terraform/modules/hopscotch_audio_support/main.tf
+++ terraform/modules/hopscotch_audio_support/main.tf
@@ -219,7 +219,7 @@ data "google_iam_policy" "storage_audiorecord" {
 
 resource "google_storage_bucket_iam_policy" "audiorecord" {
   bucket = google_storage_bucket.audiorecord.name
-  policy_data = data.google_iam_policy.storage_audiorecord
+  policy_data = data.google_iam_policy.storage_audiorecord.policy_data
 }
 
 resource "google_service_account_iam_member" "run_serviceaccounttokencreator" {
