872a6e958f6160caea7085664bfd7eb5b3984143
Kaito Udagawa <umireon@gmail.com>
2024-08-14 23:34:23 +0900

    Update main.tf

--- terraform/modules/hopscotch_audio_support/main.tf
+++ terraform/modules/hopscotch_audio_support/main.tf
@@ -93,7 +93,7 @@ resource "google_clouddeploy_target" "default_prod" {
 
 resource "google_clouddeploy_delivery_pipeline" "default" {
   location = var.region
-  name = var.short_name
+  name = "${var.short_name}-default"
   serial_pipeline {
     stages {
       target_id = google_clouddeploy_target.default_prod.name
