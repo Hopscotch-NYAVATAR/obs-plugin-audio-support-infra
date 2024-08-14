47a6931ef2f732e8b0e9587d0d3d32d1707a3534
Kaito Udagawa <umireon@gmail.com>
2024-08-14 12:23:45 +0900

    Add issueIndefiniteAccessToken

---
+++ packages/backend/env.ts
@@ -0,0 +1,7 @@
+export function readFromEnv(name: string) {
+  const value = process.env[name];
+  if (value == null) {
+    throw new Error(`Environment variable ${name} is not defined!`);
+  }
+  return value
+}
--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,6 +1,21 @@
 import express from "express";
 
+import { JWTHeader, JWTPayload, signJWT } from "./jwt";
+
 const app = express();
+
+app.post("/issueIndefiniteAccessToken", (_, res) => {
+  const header = {
+    alg: "ES256",
+    typ: "JWT",
+  } satisfies JWTHeader;
+
+  const payload = {} satisfies JWTPayload;
+
+  const indefiniteAccessToken = signJWT(header, payload);
+
+  res.send(indefiniteAccessToken);
+});
 
 app.post("/audioRecordingAccessToken", (_, res) => {
   res.send("{}");
